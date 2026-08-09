74eecde4a47317f3e0916f82529648bf00721093
Monick Évelyn <monick.evelyn.tech@gmail.com>
2026-08-09 18:03:30 -0300

    fix: Classe Bilhete e imports de sistema

--- src/service/SistemaSorteio.java
+++ src/service/SistemaSorteio.java
@@ -3,6 +3,10 @@ package service;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import model.Comprador;
+import model.Vendedor;
+import model.interfaces.Sorteavel;
+
 public class SistemaSorteio {
 	private HashMap<String, Sorteavel> itens;
 	private ArrayList<Vendedor> vendedores;
