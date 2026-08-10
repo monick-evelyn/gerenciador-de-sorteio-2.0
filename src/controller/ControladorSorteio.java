afe56464bb8098520a0c0f022e1ebb0334d35a7a
Monick Évelyn <monick.evelyn.tech@gmail.com>
2026-08-10 00:44:16 -0300

    style: formatacao controlador

--- src/controller/ControladorSorteio.java
+++ src/controller/ControladorSorteio.java
@@ -12,34 +12,35 @@ public class ControladorSorteio {
 	private SistemaSorteio sistema;
 
 	public ControladorSorteio() {
-		sistema=new SistemaSorteio();
-	}
-	
-	public boolean cadastrarSorteio(int codigoSorteio, TipoSorteio tipo, String premio, double valorBilhete, double valorParaArrecadar) {
-		return sistema.cadastrarSorteio(codigoSorteio, tipo,premio, valorBilhete, valorParaArrecadar);
+		sistema = new SistemaSorteio();
 	}
 
-	
+	public boolean cadastrarSorteio(int codigoSorteio, TipoSorteio tipo, String premio, double valorBilhete,
+			double valorParaArrecadar) {
+		return sistema.cadastrarSorteio(codigoSorteio, tipo, premio, valorBilhete, valorParaArrecadar);
+	}
+
 	public boolean cadastrarVendedor(String cpf, String nome, String telefone) {
 		return sistema.cadastrarVendedor(cpf, nome, telefone);
 	}
-	
+
 	public boolean cadastrarComprador(String cpf, String nome, String telefone) {
 		return sistema.cadastrarComprador(cpf, nome, telefone);
 	}
-	
-	public boolean cadastrarBilhete(int codigoSorteio, int numero, Vendedor vendedor, Comprador comprador, FormaDePagamento formaPagamento) {
+
+	public boolean cadastrarBilhete(int codigoSorteio, int numero, Vendedor vendedor, Comprador comprador,
+			FormaDePagamento formaPagamento) {
 		return sistema.cadastrarBilhete(codigoSorteio, numero, vendedor, comprador, formaPagamento);
 	}
-	
+
 	public Vendedor buscarVendedorPorCPF(String cpf) {
 		return sistema.buscarVendedorPorCPF(cpf);
 	}
-	
+
 	public Comprador buscarCompradorPorCPF(String cpf) {
 		return sistema.buscarCompradorPorCPF(cpf);
 	}
-	
+
 	public Bilhete buscarBilhetePorCodigo(int codigoSorteio, int numeroBilhete) {
 		return sistema.buscarBilhetePorCodigo(codigoSorteio, numeroBilhete);
 	}
